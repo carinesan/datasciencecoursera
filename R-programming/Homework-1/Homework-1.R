a336a976c8f4308e44280e0046f1f91bf9aedb29
Carine San <carinesan@users.noreply.github.com>
2014-05-12 00:19:01 +0200

    Update Homework-1.R

--- R-programming/Homework-1/Homework-1.R
+++ R-programming/Homework-1/Homework-1.R
@@ -1,12 +1,15 @@
-#Attribute the file to a variable
+#Answers of Homework 1
+#by Carine San
+
+#Read the file
 hw1_data <- read.csv("hw1_data.csv")
 
 #Question 11
-#In the dataset provided for this Quiz, what are the column names of the dataset?
+#What are the column names of the dataset?
 names(hw1_data)
 
 #Question 12
-#Extract the first 2 rows of the data frame and print them to the console. What does the output look like ?
+#Extract the first 2 rows of the data frame and print them to the console.
 hw1_data[1:2, ] #or
 head(hw1_data)
 
@@ -15,7 +18,7 @@ head(hw1_data)
 nrow(hw1_data)
 
 #Question 14
-#Extract the last 2 rows of the data frame and print them to the console. What does the output look like ?
+#Extract the last 2 rows of the data frame and print them to the console.
 hw1_data[152:153, ] #or
 tail(hw1_data)
 
@@ -29,11 +32,13 @@ missing.values <- is.na(hw1_data$Ozone)
 length(missing.values[missing.values==TRUE])
 
 #Question 17
-#What is the mean of the Ozone column in this dataset ? Exclude missing values (coded as NA) from this calculation.
+#What is the mean of the Ozone column in this dataset ?
+#Exclude missing values (coded as NA) from this calculation.
 mean(hw1_data$Ozone,na.rm=TRUE)
 
 #Question 18
-#Extract the subset of rows of the data frame where Ozone values are above 31 and Temp values are above 90. What is the mean of Solar.R in this subset ?
+#Extract the subset of rows of the data frame where Ozone values are above 31 and Temp values are above 90. 
+#What is the mean of Solar.R in this subset ?
 right.values <- subset(hw1_data, subset = hw1_data$Ozone > 31 & hw1_data$Temp > 90)
 mean(right.values$Solar.R)
 
