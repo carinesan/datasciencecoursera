9a333537a2a9baa70f5b5f7f74562bbb6e1fc123
Carine San <carinesan@users.noreply.github.com>
2014-05-11 22:48:59 +0200

    Update complete.R

--- R-programming/Assignment-1/complete.R
+++ R-programming/Assignment-1/complete.R
@@ -15,9 +15,4 @@ complete <- function(directory, id=1:332){
                 nobs <- append(nobs,nob)
         }
         df <- data.frame(id,nobs)
-#         print(df)
 }
-#  complete ("specdata",1)
-# complete ("specdata",c(2,4,8,10,12))
-# complete ("specdata",30:25)
-# complete ("specdata",3)
