3a2d31ada2a552dbe09737685632024c499d27f7
Carine San <carinesan@users.noreply.github.com>
2014-05-11 22:49:57 +0200

    Update pollutantmean.R

--- R-programming/Assignment-1/pollutantmean.R
+++ R-programming/Assignment-1/pollutantmean.R
@@ -15,9 +15,3 @@ pollutantmean <- function(directory, pollutant, id = 1:332){
         mean(z)
         
 }
-pollutantmean("specdata","sulfate",1:10)
-pollutantmean("specdata","nitrate",70:72)
-pollutantmean("specdata","nitrate",23)
-
-
-
